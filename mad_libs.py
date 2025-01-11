1cd5ca593238224e157d9437d6d7f200b52c23a2
Gonzalo Julián Acero <gonzalo.acero@hotmail.com>
2025-01-11 12:53:06 -0300

    Final Commit
    Juego terminado. Ejercicio 1 terminado.

---
+++ mad_libs.py
@@ -0,0 +1,11 @@
+# Mad Libs (Historias Locas)
+# Concatenando cadenas de caracteres.
+
+adj= input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural1 = input("Sustantivo (plural): ")
+
+madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} en YouTube y alcanza tus {sustantivo_plural1}"
+
+print(madlib)
